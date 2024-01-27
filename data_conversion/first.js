84a04b4479ac7e217c952693cdc2127c3631a162
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-01-27 16:24:56 +0545

    javascript_symbioids

---
+++ data_conversion/first.js
@@ -0,0 +1,13 @@
+let data = true;
+console.log(typeof(data))
+let valueOfNumber = Number(data)
+console.log(valueOfNumber)
+
+let isLogIn = '';
+let value = Boolean(isLogIn)
+console.log(value);
+
+let somenumber = 99
+let check = String(somenumber)
+console.log(check);
+console.log(typeof check);
---
+++ javascript/second.js
@@ -0,0 +1,5 @@
+// console.log("2">1)
+// console.log("02" > 1);
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
