c0213e2a5ebce2800434dfe7d8138535432d6a93
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-01-27 22:39:28 +0545

    add first time

---
+++ javascript/string.js
@@ -0,0 +1,9 @@
+let name = "paras";
+let repoCout  = 6;
+console.log(`My name is ${name.toUpperCase()} shrestha and my repo 
+count is ${repoCout}`)
+const nameAnother = new String('Shrestha')
+console.log(nameAnother[0])
+console.log(nameAnother.toUpperCase())
+console.log(nameAnother.__proto__)
+console.log(name[1]);
