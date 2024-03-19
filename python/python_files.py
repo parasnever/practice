fbc2bc4984230c804fc1f3820b30c976086e259d
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-03-19 10:03:22 +0545

    multiline text

--- python/python_files.py
+++ python/python_files.py
@@ -1,2 +1,3 @@
 with open('python/newfile.txt', 'w') as file:
-    file.write("This is a new file created.")
+    # file.write("This is a new file created.")
+    file.writelines(["this is a file.", "\n this is a another file."])
