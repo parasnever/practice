ec0d3c37759606f929a51298c20d93c6f4ec34e2
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-03-19 10:11:10 +0545

    append the line

--- python/python_files.py
+++ python/python_files.py
@@ -1,3 +1,5 @@
-with open('python/newfile.txt', 'w') as file:
-    # file.write("This is a new file created.")
-    file.writelines(["this is a file.", "\n this is a another file."])
+# with open('python/newfile.txt', 'w') as file:
+#     # file.write("This is a new file created.")
+#     file.writelines(["this is a file.", "\n this is a another file."])
+with open('python/newfile.txt', 'a') as file:
+    file.writelines(["this is first line2", " \n this is second line"])
